c33e038bcaabe68bf3e4955f0eda64d5a6534bef
marioviana07 <mario_viana_2005@yahoo.com>
2020-06-22 20:25:45 -0700

    JSON

---
+++ assets/js/homepage.js
@@ -0,0 +1,24 @@
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+// fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//     response.json().then(function(data) {
+//         console.log(data);
+//         console.log("inside", response);
+//     });
+// });
+// console.log("outside");
+
+//     var response = fetch("https://api.github.com/users/octocat/repos");
+//     console.log(response);
+// };
+
+getUserRepos();
